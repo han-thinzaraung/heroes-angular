fe75530586aca48f5e3b6da0eb07bd2b6deb6c2a
hanthinzaraung <hanthinzar1@gmail.com>
2024-02-21 19:25:42 +0630

    heroes-update

---
+++ heroes/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  // standalone: true,
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  // imports: [HeroesComponent]
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
